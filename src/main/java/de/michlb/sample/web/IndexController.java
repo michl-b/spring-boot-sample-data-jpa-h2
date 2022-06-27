f0128c731cf86144fbfce4cf992fd0df5d726fe2
leomahesh <99608166+leomahesh@users.noreply.github.com>
2022-06-27 09:52:34 +0400

    import commit

--- src/main/java/de/michlb/sample/web/IndexController.java
+++ src/main/java/de/michlb/sample/web/IndexController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+//import java.util.List;
 
 /**
  * Created by mbart on 28.02.2016.
